9e0c5ac6c9f3d92944a51cfeca2b5a646eb49a82
Davidgomez2405 <132088993+Davidgomez2405@users.noreply.github.com>
2023-06-16 22:31:12 -0500

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,44 @@
+let animales = [
+
+    {
+        nombre: "Jorge",
+      tipo: "Perro",
+      raza: "Pug",
+      color: "Blanco",
+      edad: 4
+    },
+    
+    {
+        nombre: "Nala",
+      tipo: "Perro",
+      raza: "Bulldog",
+      color: "Cafe",
+      edad: 8
+    },
+
+    {
+        nombre: "Jerry",
+      tipo: "Gato",
+      raza: "Persa",
+      color: "Negro",
+      edad: 10
+    },
+
+    {
+        nombre: "Tommy",
+      tipo: "Gato",
+      raza: "Siames",
+      color: "Blanco con manchas negras",
+      edad: 2
+    },
+
+    {
+        nombre: "Tita",
+      tipo: "Perro",
+      raza: "Bulldog frances",
+      color: "Cafe",
+      edad: 8
+    },
+    ]
+    
+    console.log(animales);
